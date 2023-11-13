e91503b7ddd8ad4345888e5e391c06074317b3c5
Zbindev <zbindev@gmail.com>
2023-11-13 14:43:58 -0600

    SDL2
    I decided to just use SDL2 because I felt like just using SDL2.

--- Turnz/src/main.cpp
+++ Turnz/src/main.cpp
@@ -1,10 +1,15 @@
+#include <iostream>
 #include <SDL.h>
 
-#include "math.h"
+int main(int argc, char** argv) {
 
-int main() {
-
+	if (SDL_Init(SDL_INIT_EVERYTHING)!=0)
+		std::cerr << "SDL failed to initialize";
+	if (SDL_CreateWindow("Turnz | By : Joel Zbinden", 0, 0, 640, 480, SDL_WINDOW_OPENGL) != 0)
+		std::cerr << "SDL window failed to open";
 	
 
+
 	return 0;
+
 }
