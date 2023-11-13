7ca11c71475510c8216314e1a579d81f4fbaa0ac
Zbindev <zbindev@gmail.com>
2023-11-13 15:12:19 -0600

    Figured out Custom Operators

--- Turnz/src/main.cpp
+++ Turnz/src/main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
+#include <string>
 #include <SDL.h>
+
+#include "math/math.h"
+
+using namespace Math;
+
+
 
 int main(int argc, char** argv) {
 
-	if (SDL_Init(SDL_INIT_EVERYTHING)!=0)
-		std::cerr << "SDL failed to initialize";
-	if (SDL_CreateWindow("Turnz | By : Joel Zbinden", 0, 0, 640, 480, SDL_WINDOW_OPENGL) != 0)
-		std::cerr << "SDL window failed to open";
-	
-
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+		std::cerr << "SDL failed to initialize \n";
+	if (SDL_CreateWindow("Turnz | By : Joel Zbinden", 200, 200, 640, 480, SDL_WINDOW_OPENGL) == 0)
+		std::cerr << "SDL window closed \n";
 
 	return 0;
 
 }
+
+
+
---
+++ Turnz/src/math/math.cpp
@@ -0,0 +1,12 @@
+#include "math.h"
+
+namespace Math {
+
+	// Vector2 Inplementation
+	Vector2::Vector2(float _x, float _y) {
+		x = _x;
+		y = _y;
+	}
+	Vector2::~Vector2(){}
+
+}
