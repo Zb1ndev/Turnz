449cccae23dfc4aa1aaa8237b8bc7a6e76c5c571
Zbindev <zbindev@gmail.com>
2023-11-13 20:34:44 -0600

    Put a blob on the screen

--- Turnz/src/main.cpp
+++ Turnz/src/main.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 #include <string>
 #include <SDL.h>
-
 #include "math/math.h"
-
-using namespace Math;
-
-
 
 int main(int argc, char** argv) {
 
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-		std::cerr << "SDL failed to initialize \n";
-	if (SDL_CreateWindow("Turnz | By : Joel Zbinden", 200, 200, 640, 480, SDL_WINDOW_OPENGL) == 0)
-		std::cerr << "SDL window closed \n";
+	SDL_Init(SDL_INIT_EVERYTHING);
+	auto gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+	auto gScreenSurface = SDL_GetWindowSurface(gWindow);
+	auto gHelloWorld = SDL_LoadBMP("C:/dev/Turnz/Turnz/src/Blob.bmp");
+	SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+	SDL_UpdateWindowSurface(gWindow);
+
+	SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
+
+	SDL_FreeSurface(gHelloWorld);
+	gHelloWorld = NULL;
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+	SDL_Quit();
 
 	return 0;
 
--- Turnz/src/math/math.h
+++ Turnz/src/math/math.h
@@ -2,6 +2,8 @@
 
 namespace Math {
 
+	float Lerp(float a, float b, float t);
+
 	class Vector2 {
 
 	public:
@@ -10,6 +12,7 @@ namespace Math {
 		float y;
 
 		Vector2(float _x, float _y);
+		Vector2 Lerp(Vector2 a, Vector2 b, float t);
 		~Vector2();
 
 		Vector2& operator+=(const Vector2& rhs) {
@@ -20,7 +23,7 @@ namespace Math {
 
 		}
 
-	};
+	}; 
 	inline Vector2 operator+(const Vector2& lhs, const Vector2& rhs) {
 
 		Vector2 result = lhs;
@@ -28,5 +31,35 @@ namespace Math {
 		return result;
 
 	}
+
+	class Vector2Int {
+
+	public:
+
+		int x;
+		int y;
+
+		Vector2Int(int _x, int _y);
+		~Vector2Int();
+
+		Vector2Int& operator+=(const Vector2Int& rhs) {
+
+			this->x = this->x + rhs.x;
+			this->y = this->y + rhs.y;
+			return *this;
+
+		}
+
+	};
+	inline Vector2Int operator+(const Vector2Int& lhs, const Vector2Int& rhs) {
+
+		Vector2Int result = lhs;
+		result += rhs;
+		return result;
+
+	}
+
+	
+
 
 }
